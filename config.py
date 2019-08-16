8df738a334733c1c98bd9d2f3fcc61b6cbe4d8b0
Ouroboros-analytics <42299955+Ouroboros-analytics@users.noreply.github.com>
2019-08-16 16:40:58 -0500

    Update .gitignore

--- config.py
+++ config.py
@@ -1,2 +1,6 @@
 # You thought I messed up, didn't you?
 # https://media.giphy.com/media/a6OnFHzHgCU1O/giphy.gif
+
+# Here's the format you'll need for your keys
+owm_key = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+gmaps_key = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
